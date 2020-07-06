a571d3d314553c641ef8291f4b066b29cd857e1d
weilyuwang <weilyuwang@gmail.com>
2020-07-06 12:15:55 -0400

    :alembic: delete lead

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_LEADS } from "./types";
+import { GET_LEADS, DELETE_LEAD } from "./types";
 
 // GET LEADS
 export const getLeads = () => (dispatch) => {
@@ -9,6 +9,19 @@ export const getLeads = () => (dispatch) => {
       dispatch({
         type: GET_LEADS,
         payload: res.data,
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+// DELETE LEAD
+export const deleteLeads = (id) => (dispatch) => {
+  axios
+    .delete(`/api/leads/${id}/`)
+    .then((res) => {
+      dispatch({
+        type: DELETE_LEAD,
+        payload: id,
       });
     })
     .catch((err) => console.log(err));
--- leadmanager/frontend/src/reducers/leads.js
+++ leadmanager/frontend/src/reducers/leads.js
@@ -1,4 +1,5 @@
-import { GET_LEADS } from "../actions/types";
+import { GET_LEADS, DELETE_LEAD } from "../actions/types";
+
 const initialState = {
   leads: [],
 };
@@ -9,6 +10,11 @@ export default function (state = initialState, action) {
       return {
         ...state,
         leads: action.payload,
+      };
+    case DELETE_LEAD:
+      return {
+        ...state,
+        leads: state.leads.filter((lead) => lead.id !== action.payload),
       };
     default:
       return state;
