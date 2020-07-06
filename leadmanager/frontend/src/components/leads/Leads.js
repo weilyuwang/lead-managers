5efe88c628a2c246232a853a8297ff9d3507af3e
weilyuwang <weilyuwang@gmail.com>
2020-07-06 12:03:21 -0400

    :fire: show leads data in a table

--- leadmanager/frontend/src/components/leads/Leads.js
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -15,9 +15,33 @@ export class Leads extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Lead List</h1>
-      </div>
+      <>
+        <h2>Leads</h2>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Message</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.leads.map((lead) => (
+              <tr key={lead.id}>
+                <td>{lead.id}</td>
+                <td>{lead.name}</td>
+                <td>{lead.email}</td>
+                <td>{lead.message}</td>
+                <td>
+                  <button className="btn btn-danger btn-sm">Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </>
     );
   }
 }
