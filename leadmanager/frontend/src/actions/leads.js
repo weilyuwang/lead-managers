753e252b8b40cd95cc7b6ad8ec8acdb63ca192b8
weilyuwang <weilyuwang@gmail.com>
2020-07-06 12:59:21 -0400

    :zap: wire up add lead func

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_LEADS, DELETE_LEAD } from "./types";
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from "./types";
 
 // GET LEADS
 export const getLeads = () => (dispatch) => {
@@ -22,6 +22,19 @@ export const deleteLead = (id) => (dispatch) => {
       dispatch({
         type: DELETE_LEAD,
         payload: id,
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+// ADD LEAD
+export const addLead = (lead) => (dispatch) => {
+  axios
+    .post("/api/leads/", lead)
+    .then((res) => {
+      dispatch({
+        type: ADD_LEAD,
+        payload: res.data,
       });
     })
     .catch((err) => console.log(err));
--- leadmanager/frontend/src/components/leads/Form.js
+++ leadmanager/frontend/src/components/leads/Form.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { addLead } from "../../actions/leads";
 
 export class Form extends Component {
   state = {
@@ -7,11 +10,17 @@ export class Form extends Component {
     message: "",
   };
 
+  static propTypes = {
+    addLead: PropTypes.func.isRequired,
+  };
+
   onChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
   onSubmit = (e) => {
     e.preventDefault();
-    console.log("submit");
+    const { name, email, message } = this.state;
+    const lead = { name, email, message };
+    this.props.addLead(lead);
   };
 
   render() {
@@ -61,4 +70,4 @@ export class Form extends Component {
   }
 }
 
-export default Form;
+export default connect(null, { addLead })(Form);
