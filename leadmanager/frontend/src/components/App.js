22da4a1b698b53e94e4ef2851c032886857f4ca8
weilyuwang <weilyuwang@gmail.com>
2020-07-06 11:59:11 -0400

    :zap: leads reducer and action

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -3,15 +3,20 @@ import ReactDOM from "react-dom";
 import Header from "./layout/Header";
 import Dashboard from "./leads/Dashboard";
 
+import { Provider } from "react-redux";
+import store from "../store";
+
 class App extends Component {
   render() {
     return (
-      <>
-        <Header />
-        <div className="container">
-          <Dashboard />
-        </div>
-      </>
+      <Provider store={store}>
+        <>
+          <Header />
+          <div className="container">
+            <Dashboard />
+          </div>
+        </>
+      </Provider>
     );
   }
 }
--- leadmanager/frontend/src/components/leads/Leads.js
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -1,6 +1,18 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { getLeads } from "../../actions/leads";
 
 export class Leads extends Component {
+  static propTypes = {
+    leads: PropTypes.array.isRequired,
+  };
+
+  componentDidMount() {
+    // getLeads() calls backend api to fetch all leads and dispatch GET_LEADS action
+    this.props.getLeads();
+  }
+
   render() {
     return (
       <div>
@@ -10,4 +22,8 @@ export class Leads extends Component {
   }
 }
 
-export default Leads;
+const mapStateToProps = (state) => ({
+  leads: state.leadReducer.leads,
+});
+
+export default connect(mapStateToProps, { getLeads })(Leads);
